b3cee09775d6008403f2983cde0236d1c6479ac1
Eky <zx21student301@colegiovalledelmiro.es>
2022-11-03 13:56:27 +0100

    03/11

--- datosRobots/js/datos.js
+++ datosRobots/js/datos.js
@@ -1,6 +1,5 @@
 onload = principal
-
-let cuerpo = document.getElementById("cuerpo");	
+	
 let img = document.createElement("img");
 let boton = document.createElement("button");
 let iden = document.createElement("p");
@@ -8,12 +7,21 @@ let nacim = document.createElement("p");
 let ciudad = document.createElement("p");
 
 function principal() {
-	boton.setAttribute("onclick","pasarRobot()");
+
+	let cuerpo = document.getElementById("cuerpo");
+
+	cuerpo.appendChild(img);
+	cuerpo.appendChild(iden);
+	cuerpo.appendChild(nacim);
+	cuerpo.appendChild(ciudad);
+	cuerpo.appendChild(boton);
+
 	pasarRobot();
+	boton.setAttribute("onclick","pasarRobot()");
 }
 
 function pasarRobot(){
-		//*********************************
+	//*********************************
 	// configuracion y objetos	
 
 	//crear el objeto XMLHttpRequest para acceder al servidor
@@ -25,21 +33,18 @@ function pasarRobot(){
 
 		//evaluar la respuesta del servidor
 		if(this.readyState == 4 && this.status == 200){
+
 			let datos = JSON.parse(this.responseText);
 
-			img.setAttribute("src", "\""+datos.avatar+"\"");
+			img.setAttribute("src", datos.avatar);
+			
+			iden.innerHTML = "Identificación: "+datos.first_name + " " + datos.last_name;
+			
+			nacim.innerHTML = "Nacimiento: "+datos.date_of_birth;
+			
+			ciudad.innerHTML = "Ciudad: "+datos.address.city;
 
-			iden.innerHTML = datos.first_name + " " + datos.last_name
-			cuerpo.appendChild(iden);
-
-			nacim.innerHTML = datos.date_of_birth
-			cuerpo.appendChild(nacim);
-
-			ciudad.innerHTML = datos.address-city
-			cuerpo.appendChild(ciudad);
-
-			boton.innerHTML = "Siguiente"
-			cuerpo.appendChild(boton);
+			boton.innerHTML = "Siguiente";
 		}
 	}
 
